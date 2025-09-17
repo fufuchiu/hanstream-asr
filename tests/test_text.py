3d5aa62da2d1b9d77d44e3d46022372f51fa189f
Chen Yuxuan <233662060+fufuchiu@users.noreply.github.com>
2025-09-17 11:15:01 +0800

    test(text): normalize spaces

--- tests/test_text.py
+++ tests/test_text.py
@@ -5,3 +5,7 @@ from hanstream import text as m
 
 def test_normalize_ascii_case():
     assert m.normalize('Hello WORLD') == 'hello world'
+
+
+def test_normalize_spaces():
+    assert m.normalize(' a  b ') == 'a b'
