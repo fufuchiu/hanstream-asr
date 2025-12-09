8cc88af9c16f02eaa4fefc8ca0a0a4c5ec324901
Chen Yuxuan <233662060+fufuchiu@users.noreply.github.com>
2025-12-09 21:06:55 +0800

    test(features): delta ramp

--- tests/test_features.py
+++ tests/test_features.py
@@ -30,3 +30,7 @@ def test_cmvn_constant():
 
 def test_delta_constant():
     assert m.delta([[3], [3], [3]]).tolist() == [[0], [0], [0]]
+
+
+def test_delta_ramp():
+    assert m.delta([[0], [1], [2]], 1).ravel().tolist() == [0.5, 1, 0.5]
