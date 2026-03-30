a1f64069762b9f0ddab28b143b326739c0a6e9ea
Chen Yuxuan <233662060+fufuchiu@users.noreply.github.com>
2026-03-30 21:15:31 +0800

    test(metrics): edit insert

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -8,3 +8,10 @@ def test_edit_empty():
     assert (r.substitutions, r.deletions, r.insertions, r.hits) == (0, 0, 0, 0)
     assert r.errors == 0
     assert r.reference_length == 0
+
+
+def test_edit_insert():
+    r = m.edit_counts('', 'ab')
+    assert (r.substitutions, r.deletions, r.insertions, r.hits) == (0, 0, 2, 0)
+    assert r.errors == 2
+    assert r.reference_length == 0
