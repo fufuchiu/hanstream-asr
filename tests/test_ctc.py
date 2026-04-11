a61f8ae8d71e742a97dcfc68d2883169a0884b42
Chen Yuxuan <233662060+fufuchiu@users.noreply.github.com>
2026-04-11 20:04:00 +0800

    test(ctc): collapse separated

--- tests/test_ctc.py
+++ tests/test_ctc.py
@@ -14,3 +14,7 @@ def test_collapse_blank():
 
 def test_collapse_repeat():
     assert m.collapse([1, 1, 1], 0) == (1,)
+
+
+def test_collapse_separated():
+    assert m.collapse([1, 0, 1], 0) == (1, 1)
