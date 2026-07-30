fe905172bd9a82f75fa75612c35f7da97387ec12
Chen Yuxuan <233662060+fufuchiu@users.noreply.github.com>
2026-07-30 11:23:18 +0800

    test(streaming): zero start

--- tests/test_streaming.py
+++ tests/test_streaming.py
@@ -6,3 +6,8 @@ from hanstream import streaming as m
 def test_zero_frame():
     with pytest.raises(ValueError):
         m.PCMFramer(0)
+
+
+def test_zero_start():
+    with pytest.raises(ValueError):
+        m.EndpointDetector(start_frames=0)
