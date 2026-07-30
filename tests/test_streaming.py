68cdfcbaa96bccfe8eb04ee6dc1334cbef85e4d5
Chen Yuxuan <233662060+fufuchiu@users.noreply.github.com>
2026-07-30 21:52:01 +0800

    test(streaming): zero silence

--- tests/test_streaming.py
+++ tests/test_streaming.py
@@ -11,3 +11,8 @@ def test_zero_frame():
 def test_zero_start():
     with pytest.raises(ValueError):
         m.EndpointDetector(start_frames=0)
+
+
+def test_zero_silence():
+    with pytest.raises(ValueError):
+        m.EndpointDetector(silence_frames=0)
